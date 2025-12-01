486bc36fe353ece896b7d05810644108ad5de0d7
lioox <lioox@liooxdeMacBook-Air.local>
2025-12-01 17:34:00 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,14 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
+document.addEventListener('DOMContentLoaded', function () {
+  const options = {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    loop: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
-
-})
+  bulmaCarousel.attach('.carousel', options);
+});
